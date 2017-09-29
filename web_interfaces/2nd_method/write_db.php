0e908791fa5aa1888b7d799e07e4d5e292f69c7d
Amaury Zarzelli <amaury.zarzelli@gmail.com>
2017-09-29 09:48:44 +0200

    Update write_db.php

--- web_interfaces/2nd_method/write_db.php
+++ web_interfaces/2nd_method/write_db.php
@@ -1,23 +1,27 @@
 <?php
-	$id1 = $_GET["id1"];
-	$pano1 = $_GET["pano1"];
-	$id2 = $_GET["id2"];
-	$pano2 = $_GET["pano2"];
+                $id1 = $_GET["id1"];
+                $pano1 = $_GET["pano1"];
+                $id2 = $_GET["id2"];
+                $pano2 = $_GET["pano2"];
 
-	$winner = $_GET["winner"];
+                $winner = $_GET["winner"];
 
-	$line = array($id1, $id2, $pano1, $pano2, $winner);
+                $line = array($id1, $id2, $pano1, $pano2, $winner);
 
-	$handle = fopen("votes.csv", "a");
-	fputcsv($handle, $line);
+                $handle = fopen("duels.csv", "a");
 
-	fclose($handle);
+                fputcsv($handle, $line);
+                fclose($handle);
+ 
+                $images = glob('../walkimages/*.jpg');
 
-	$images = glob('../voteimages/*.jpg');
+ 
+                $cnt = count($images);
+                $ind1 = rand(0, $cnt-1);
+                $ind2 = rand(0, $cnt-1);
 
-	$cnt = count($images)-1;
-	$ind1 = rand(0, $cnt);
-	$ind2 = rand(0, $cnt);
-
-	echo json_encode(array('id1' => $ind1, 'id2' => $ind2));
+                 while($ind1 == $ind2){
+                                $ind2 = rand(0,$cnt-1);
+                }   
+                echo json_encode(array('id1' => $ind1, 'id2' => $ind2));
 ?>
